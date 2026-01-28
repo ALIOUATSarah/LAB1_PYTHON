978efed66315ed85b2f4c3ccae521ee28e34a30a
ALIOUATSarah <147725519+ALIOUATSarah@users.noreply.github.com>
2026-01-28 11:29:00 +0400

    added task 1

---
+++ tuto11.py
@@ -0,0 +1,15 @@
+
+#THE SOLUTION
+
+#ASK THE USER FOR INPUT
+n1=float(input("enter First decimal number"))
+n2=float(input("enter Scound decimal number"))
+n3=float(input("enter Thired decimal number"))
+#PERFORME THE REQUIRED CALCULATIONS
+r1=n1*(n2+n3)/2
+r2=n1-(n2+n2*n2)/n3
+r3=(n1-n2)*n3/2
+#DISPLAY THE RESULTS TO THE USER
+print("the first result",r1)
+print("the scound result",r2)
+print("the third result",r3)
